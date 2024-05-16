025f62afd4fffb55b152765ebf0ae8d1e5296bab
stephen-c-perry <stephen.christian.perry@gmail.com>
2024-05-16 08:16:42 -0500

    beancreation bug fixed

--- src/main/java/com/skillstorm/repositories/WarehouseRepository.java
+++ src/main/java/com/skillstorm/repositories/WarehouseRepository.java
@@ -2,7 +2,9 @@ package com.skillstorm.repositories;
 
 import com.skillstorm.models.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
     
 }
